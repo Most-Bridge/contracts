f4333043f4f779f940106007b7d021f973c0ceca
jackchinski <marcin.jacz@gmail.com>
2024-07-17 13:02:41 -0400

    deploy script

---
+++ script/DeployPaymentRegistry.s.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import {Script} from "forge-std/Script.sol";
+import {PaymentRegistry} from "../src/PaymentRegistry.sol";
+
+contract DeployPaymentRegistry is Script {
+    function run() external {
+        vm.startBroadcast();
+        new PaymentRegistry();
+        vm.stopBroadcast();
+    }
+}
+
+// run using
+// forge script script/DeployPaymentRegistry.s.sol:DeployPaymentRegistry --rpc-url https://sepolia.infura.io/v3/580a6a4667464912b43ef356b016839c --broadcast --private-key $SEPOLIA_TEST_PRIVATE_KEY
