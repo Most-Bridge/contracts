2c3a1fc2f971c8db11e94c436e7655832332d158
jackchinski <marcin.jacz@gmail.com>
2024-08-29 16:09:51 -0400

    op etherscan specific api key

--- script/escrow/verify_escrow.sh
+++ script/escrow/verify_escrow.sh
@@ -3,5 +3,7 @@
 # load env vars 
 source .env && \
 
+export ETHERSCAN_API_KEY=$OP_ETHERSCAN_API_KEY
+
 # *NOTE* change contract address with each deployment
-forge verify-contract --chain-id 11155420 --compiler-version 0.8.26  0x248DA3C9904a3e1CdF90e27f35b59235eB3eEDB5 src/contracts/Escrow.sol:Escrow
+forge verify-contract --chain-id 11155420 --compiler-version 0.8.26  0xF542a55D4F43d2557C7Ad2d4Ed664Bf555e2F1d5 src/contracts/Escrow.sol:Escrow
