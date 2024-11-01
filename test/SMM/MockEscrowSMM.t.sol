7d30c654f49a60d4d78d0db957a8699f8cefbff8
jackchinski <marcin.jacz@gmail.com>
2024-11-01 12:47:46 +0100

    refactor: update mock escrow

--- test/SMM/MockEscrowSMM.t.sol
+++ test/SMM/MockEscrowSMM.t.sol
@@ -2,7 +2,7 @@
 pragma solidity ^0.8.0;
 
 import {Test, console} from "forge-std/Test.sol";
-import {MockEscrow} from "../src/mock/MockEscrow.sol";
+import {MockEscrow} from "../../src/mock/MockEscrow.sol";
 
 contract MockEscrowTest is Test {
     MockEscrow mockEscrow;
@@ -88,46 +88,6 @@ contract MockEscrowTest is Test {
         mockEscrow.withdrawProved(100);
     }
 
-    function testGetValuesFromSlots() public {
-        // bytes32 orderIdSlot = bytes32(uint256(0x1));
-        // bytes32 dstAddressSlot = bytes32(uint256(0x2));
-        // bytes32 mmSrcAddressSlot = bytes32(uint256(0x3));
-        // bytes32 amountSlot = bytes32(uint256(0x4));
-        // uint256 blockNumber = 5;
-
-        // // Mock data
-        // uint256 expectedOrderId = 1111;
-        // address expectedDstAddress = address(0x2222);
-        // address expectedMmSrcAddress = address(0x3333);
-        // uint256 expectedAmount = 4444;
-
-        // factsRegistry.setSlotValue(address(mockEscrow), blockNumber, orderIdSlot, bytes32(uint256(expectedOrderId)));
-        // factsRegistry.setSlotValue(
-        //     address(mockEscrow), blockNumber, dstAddressSlot, bytes32(uint256(uint160(expectedDstAddress)))
-        // );
-        // factsRegistry.setSlotValue(
-        //     address(mockEscrow), blockNumber, mmSrcAddressSlot, bytes32(uint256(uint160(expectedMmSrcAddress)))
-        // );
-        // factsRegistry.setSlotValue(address(mockEscrow), blockNumber, amountSlot, bytes32(expectedAmount));
-
-        // // Expect SlotsReceived event to be emitted
-        // // vm.expectEmit(true, true, true, true);
-        // // emit mockEscrow.SlotsReceived(orderIdSlot, dstAddressSlot, mmSrcAddressSlot, amountSlot, blockNumber);  // Reference mockEscrow instance
-
-        // // get return values
-        // (bytes32 returnedOrderId, bytes32 returnedDstAddress, bytes32 returnedMmSrcAddress, bytes32 returnedAmount) =
-        //     mockEscrow.getValuesFromSlots(orderIdSlot, dstAddressSlot, mmSrcAddressSlot, amountSlot, blockNumber);
-
-        // // assert correct values
-        // assertEq(returnedOrderId, bytes32(uint256(expectedOrderId)));
-        // assertEq(returnedDstAddress, bytes32(uint256(uint160(expectedDstAddress))));
-        // assertEq(returnedMmSrcAddress, bytes32(uint256(uint160(expectedMmSrcAddress))));
-        // assertEq(returnedAmount, bytes32(expectedAmount));
-
-        // NOTE: doesn't work as expected...
-        // need a better way to mock the facts registry contract
-        // and test that it gets back proper values
-    }
 
     function testConvertBytes32ToNative() public {
         // arrange
@@ -219,7 +179,6 @@ contract MockEscrowTest is Test {
             mockEscrow.proveBridgeTransaction(orderIds[i], destinationAddress, mmSrcAddress, sendAmount);
         }
 
-        // TODO: check that balance of this contract is now worth two orders
         uint256 expectedBalance = (sendAmount + fee) * orderIds.length;
         assertEq(address(mockEscrow).balance, expectedBalance, "Contract balance does not match");
 
