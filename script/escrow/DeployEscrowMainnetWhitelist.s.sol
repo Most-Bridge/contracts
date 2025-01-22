cad7b9870410dfc2755c3da20afd68988ed07b47
lakewik <wikjezioro@op.pl>
2025-01-22 22:04:11 +0100

    Uncomment

--- script/escrow/DeployEscrowMainnetWhitelist.s.sol
+++ script/escrow/DeployEscrowMainnetWhitelist.s.sol
@@ -1,19 +1,19 @@
-// // SPDX-License-Identifier: MIT
-// pragma solidity ^0.8.0;
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
 
-// import {Script} from "forge-std/Script.sol";
-// import {EscrowWhitelist} from "../../src/contracts/whitelist/EscrowWhitelist.sol";
+import {Script} from "forge-std/Script.sol";
+import {EscrowWhitelist} from "../../src/contracts/whitelist/EscrowWhitelist.sol";
 
-// contract DeployEscrowWhitelist is Script {
-//     function run() external {
-//         address[] storage whitelistAddresses;
+contract DeployEscrowWhitelist is Script {
+    function run() external {
+        address[] storage whitelistAddresses;
 
-//         whitelistAddresses.push();
+        whitelistAddresses.push();
 
-//         vm.startBroadcast();
-//         new EscrowWhitelist(); // TODO: ADD WHITELIST ADDRESSES
-//         vm.stopBroadcast();
-//     }
-// }
+        vm.startBroadcast();
+        new EscrowWhitelist(); // TODO: ADD WHITELIST ADDRESSES
+        vm.stopBroadcast();
+    }
+}
 
-// // executable in deploy_escrow.sh
+// executable in deploy_escrow.sh
