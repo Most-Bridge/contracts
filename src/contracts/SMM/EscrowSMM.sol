d50f584ef027def2838e2afbd269cfcf77493125
lakewik <wikjezioro@op.pl>
2025-05-31 22:12:34 +0200

    Start implementing task result decommitment

--- src/contracts/SMM/EscrowSMM.sol
+++ src/contracts/SMM/EscrowSMM.sol
@@ -28,6 +28,8 @@ contract Escrow is ReentrancyGuard, Pausable {
 
     // HDP
     address public HDP_EXECUTION_STORE_ADDRESS = 0x59c0B3D09151aA2C0201808fEC0860f1168A4173;
+    bytes32 private constant HDP_EMPTY_OUTPUT_TREE_HASH =
+        0x6612f7b477d66591ff96a9e064bcc98abc36789e7a1e281436464229828f817d;
 
     // Interfaces
     IDataProcessorModule hdpExecutionStore = IDataProcessorModule(HDP_EXECUTION_STORE_ADDRESS);
@@ -96,6 +98,17 @@ contract Escrow is ReentrancyGuard, Pausable {
         bytes32 destinationChainId;
         bytes32 hdpProgramHash;
         bytes32 paymentRegistryAddress;
+    }
+
+    struct BalanceToWithdraw {
+        bytes32 tokenAddress;
+        uint256 summarizedAmount;
+    }
+
+    struct HDPTaskOutput {
+        bytes32 isOrdersFulfillmentVerified;
+        uint256 tokensBalancesArrayLength;
+        BalanceToWithdraw[] tokensBalancesArray;
     }
 
     /// Constructor
@@ -196,7 +209,8 @@ contract Escrow is ReentrancyGuard, Pausable {
     /// @param calldataOrders      Array containing the data of the orders to be proven
     /// @param _blockNumber        The point in time when all the submitted orders have been fulfilled
     /// @param _destinationChainId The chain on which the order was fulfilled
-    function proveAndWithdrawBatch(Order[] calldata calldataOrders, uint256 _blockNumber, bytes32 _destinationChainId)
+    /// @param _balancesToWithdraw Summarized amounts of each token to withdraw - token address and amount pair
+    function proveAndWithdrawBatch(Order[] calldata calldataOrders, uint256 _blockNumber, bytes32 _destinationChainId, BalanceToWithdraw[] calldata _balancesToWithdraw)
         public
         onlyRelayAddress
     {
@@ -249,10 +263,26 @@ contract Escrow is ReentrancyGuard, Pausable {
             hdpExecutionStore.getDataProcessorTaskStatus(taskCommitment) == IDataProcessorModule.TaskStatus.FINALIZED,
             "HDP Task is not finalized"
         );
+
+        // HDP task result is merkelized - we are getting Merkle Root from HDP Execution Store
+        // We need to veryfy that this merkle root matches with the data provied here
+        // First element of HDP module output array is boolean value - true if all orders in batch are verified
+        // Second element of HDP module output arrya is length of the tokens and balances array
+        // The next elements are the actual token addresses and summarized token balances repeated n-times where n is number of unique tokmens in batch
+        
+        HDPTaskOutput memory expectedHdpTaskOutput = HDPTaskOutput({
+            isOrdersFulfillmentVerified: bytes32(uint256(1)),
+            tokensBalancesArrayLength: _balancesToWithdraw.length,
+            tokensBalancesArray: _balancesToWithdraw
+            
+        });
+
+        bytes32 computedMerkleRoot = computeTaskOutputMerkleRoot(expectedHdpTaskOutput);
+
         require(
             hdpExecutionStore.getDataProcessorFinalizedTaskResult(taskCommitment)
-                == bytes32(uint256(HDPProvingStatus.PROVEN)),
-            "Unable to prove PaymentRegistry transfer execution"
+                == computedMerkleRoot,
+            "Unable to prove: merkle root mismatch"
         );
 
         // Once validated, update the status of all the orders
@@ -424,4 +454,65 @@ contract Escrow is ReentrancyGuard, Pausable {
         require(msg.sender == owner, "Caller is not the owner");
         _;
     }
+
+    // Helper functions
+    function computeTaskOutputMerkleRoot(HDPTaskOutput memory taskOutput)
+        internal
+        pure
+        returns (bytes32)
+    {
+        require(
+            taskOutput.tokensBalancesArray.length == taskOutput.tokensBalancesArrayLength,
+            "HDPTaskOutput: length mismatch"
+        );
+
+        /*
+            leaf[0] = keccak256(abi.encode(isOrdersFulfillmentVerified))
+            leaf[1] = keccak256(abi.encode(tokensBalancesArrayLength))
+            leaf[2+i] = keccak256(abi.encode(tokenAddress, summarizedAmount))
+        */
+        uint256 nLeaves = 2 + taskOutput.tokensBalancesArrayLength;
+        bytes32[] memory leaves = new bytes32[](nLeaves);
+
+        leaves[0] = keccak256(abi.encode(taskOutput.isOrdersFulfillmentVerified));
+        leaves[1] = keccak256(abi.encode(taskOutput.tokensBalancesArrayLength));
+
+        for (uint256 i = 0; i < taskOutput.tokensBalancesArray.length; ++i) {
+            BalanceToWithdraw memory b = taskOutput.tokensBalancesArray[i];
+            leaves[2 + i] = keccak256(abi.encode(b.tokenAddress, b.summarizedAmount));
+        }
+
+        return computeMerkleRoot(leaves);
+    }
+
+    function computeMerkleRoot(bytes32[] memory leaves)
+        internal
+        pure
+        returns (bytes32 root)
+    {
+        require(leaves.length > 0, "StandardMerkleTree: empty leaves");
+
+        while (leaves.length > 1) {
+            uint256 next = (leaves.length + 1) >> 1;
+            bytes32[] memory level = new bytes32[](next);
+
+            for (uint256 i = 0; i < leaves.length; i += 2) {
+                bytes32 left  = leaves[i];
+                // duplicate last element if the level has odd length
+                bytes32 right = i + 1 < leaves.length ? leaves[i + 1] : left;
+                level[i >> 1] = _hashPair(left, right);
+            }
+            leaves = level;
+        }
+        root = leaves[0];
+    }
+
+
+    function _hashPair(bytes32 a, bytes32 b) private pure returns (bytes32) {
+        return a <= b
+            ? keccak256(bytes.concat(a, b))
+            : keccak256(bytes.concat(b, a));
+    }
+
+
 }
