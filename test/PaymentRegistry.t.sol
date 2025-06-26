4e9aeef7b63a5a9cd2690e8922cb31d56768eb26
jackchinski <marcin.jacz@gmail.com>
2025-06-26 15:44:49 +0200

    refactor: adjust tests for new batch function only

--- test/PaymentRegistry.t.sol
+++ test/PaymentRegistry.t.sol
@@ -42,21 +42,48 @@ contract PaymentRegistryTest is Test {
         mockERC.mint(MMAddress, 10 ether);
     }
 
+    /// @dev Creates a customizable array of orders for testing.
+    function _createOrdersArray(
+        address _dstToken,
+        uint256 _dstAmount,
+        address _usrDstAddress,
+        uint256 _expirationTimestamp
+    ) internal view returns (PaymentRegistry.OrderFulfillmentData[] memory) {
+        PaymentRegistry.OrderFulfillmentData[] memory orders = new PaymentRegistry.OrderFulfillmentData[](1);
+        orders[0] = PaymentRegistry.OrderFulfillmentData({
+            orderId: orderId,
+            srcEscrow: srcEscrow,
+            usrSrcAddress: userSrcAddress,
+            usrDstAddress: _usrDstAddress,
+            expirationTimestamp: _expirationTimestamp,
+            srcToken: srcToken,
+            srcAmount: srcAmount,
+            dstToken: _dstToken,
+            dstAmount: _dstAmount,
+            srcChainId: srcChainId,
+            marketMakerSourceAddress: mmSrcAddress
+        });
+        return orders;
+    }
+
+    /// @dev Creates a default ETH order array.
+    function _createDefaultEthOrdersArray() internal view returns (PaymentRegistry.OrderFulfillmentData[] memory) {
+        return _createOrdersArray(dstTokenETH, dstAmount, userDstAddress, expirationTimestamp);
+    }
+
+    /// @dev Creates a default ERC20 order array.
+    function _createDefaultErc20OrdersArray(uint256 _dstAmount)
+        internal
+        view
+        returns (PaymentRegistry.OrderFulfillmentData[] memory)
+    {
+        return _createOrdersArray(address(mockERC), _dstAmount, userDstAddress, expirationTimestamp);
+    }
+
     function testFulfillmentSuccess() public {
+        PaymentRegistry.OrderFulfillmentData[] memory orders = _createDefaultEthOrdersArray();
         vm.prank(MMAddress); // mm calls
-        paymentRegistry.mostFulfillOrder{value: dstAmount}(
-            orderId,
-            srcEscrow,
-            userSrcAddress,
-            userDstAddress,
-            expirationTimestamp,
-            srcToken,
-            srcAmount,
-            dstTokenETH,
-            dstAmount,
-            srcChainId,
-            mmSrcAddress
-        );
+        paymentRegistry.mostFulfillOrders{value: dstAmount}(orders);
 
         bytes32 orderHash = keccak256(
             abi.encode(
@@ -80,129 +107,53 @@ contract PaymentRegistryTest is Test {
     }
 
     function testFulfillmentFailsIfAlreadyProcessed() public {
+        PaymentRegistry.OrderFulfillmentData[] memory orders = _createDefaultEthOrdersArray();
         vm.startPrank(MMAddress);
-        paymentRegistry.mostFulfillOrder{value: dstAmount}(
-            orderId,
-            srcEscrow,
-            userSrcAddress,
-            userDstAddress,
-            expirationTimestamp,
-            srcToken,
-            srcAmount,
-            dstTokenETH,
-            dstAmount,
-            srcChainId,
-            mmSrcAddress
-        );
+        paymentRegistry.mostFulfillOrders{value: dstAmount}(orders);
 
         vm.expectRevert("Transfer already processed");
-        paymentRegistry.mostFulfillOrder{value: dstAmount}(
-            orderId,
-            srcEscrow,
-            userSrcAddress,
-            userDstAddress,
-            expirationTimestamp,
-            srcToken,
-            srcAmount,
-            dstTokenETH,
-            dstAmount,
-            srcChainId,
-            mmSrcAddress
-        );
+        paymentRegistry.mostFulfillOrders{value: dstAmount}(orders);
         vm.stopPrank();
     }
 
     function testFulfillmentFailsIfNoValue() public {
+        PaymentRegistry.OrderFulfillmentData[] memory orders = _createDefaultEthOrdersArray();
         vm.prank(MMAddress);
-        vm.expectRevert("Native ETH: msg.value mismatch with destination amount");
-        paymentRegistry.mostFulfillOrder{value: 0}(
-            orderId,
-            srcEscrow,
-            userSrcAddress,
-            userDstAddress,
-            expirationTimestamp,
-            srcToken,
-            srcAmount,
-            dstTokenETH,
-            dstAmount,
-            srcChainId,
-            mmSrcAddress
-        );
+        vm.expectRevert("Native ETH: Transfer failed");
+        paymentRegistry.mostFulfillOrders{value: 0}(orders);
     }
 
     function testFulfillmentFailsIfWrongValue() public {
+        PaymentRegistry.OrderFulfillmentData[] memory orders =
+            _createOrdersArray(dstTokenETH, dstAmount + 1 ether, userDstAddress, expirationTimestamp);
         vm.prank(MMAddress);
-        vm.expectRevert("Native ETH: msg.value mismatch with destination amount");
-        paymentRegistry.mostFulfillOrder{value: dstAmount}(
-            orderId,
-            srcEscrow,
-            userSrcAddress,
-            userDstAddress,
-            expirationTimestamp,
-            srcToken,
-            srcAmount,
-            dstTokenETH,
-            dstAmount + 1 ether,
-            srcChainId,
-            mmSrcAddress
-        );
+        vm.expectRevert("Native ETH: Transfer failed");
+        paymentRegistry.mostFulfillOrders{value: dstAmount}(orders);
     }
 
     function testFulfillmentFailsOnExpiredOrder() public {
+        PaymentRegistry.OrderFulfillmentData[] memory orders = _createDefaultEthOrdersArray();
         vm.prank(MMAddress);
         vm.expectRevert("Cannot fulfill an expired order.");
         // warping time to expire order
         vm.warp(block.timestamp + 2 days);
-        paymentRegistry.mostFulfillOrder{value: dstAmount}(
-            orderId,
-            srcEscrow,
-            userSrcAddress,
-            userDstAddress,
-            expirationTimestamp,
-            srcToken,
-            srcAmount,
-            dstTokenETH,
-            dstAmount,
-            srcChainId,
-            mmSrcAddress
-        );
+        paymentRegistry.mostFulfillOrders{value: dstAmount}(orders);
     }
 
     function testMMSendsMoreThanBalance() public {
+        PaymentRegistry.OrderFulfillmentData[] memory orders =
+            _createOrdersArray(dstTokenETH, 20 ether, userDstAddress, expirationTimestamp);
         vm.prank(MMAddress);
         vm.expectRevert();
-        paymentRegistry.mostFulfillOrder{value: 20 ether}(
-            orderId,
-            srcEscrow,
-            userSrcAddress,
-            userDstAddress,
-            expirationTimestamp,
-            srcToken,
-            srcAmount,
-            dstTokenETH,
-            dstAmount,
-            srcChainId,
-            mmSrcAddress
-        );
+        paymentRegistry.mostFulfillOrders{value: 20 ether}(orders);
     }
 
     function testFulfillmentPassesOnERC20() public {
+        PaymentRegistry.OrderFulfillmentData[] memory orders = _createDefaultErc20OrdersArray(dstAmount);
         // send a good erc20 transfer
         vm.startPrank(MMAddress);
         mockERC.approve(address(paymentRegistry), dstAmount);
-        paymentRegistry.mostFulfillOrder{value: 0}(
-            orderId,
-            srcEscrow,
-            userSrcAddress,
-            userDstAddress,
-            expirationTimestamp,
-            srcToken,
-            srcAmount,
-            address(mockERC),
-            dstAmount,
-            srcChainId,
-            mmSrcAddress
-        );
+        paymentRegistry.mostFulfillOrders{value: 0}(orders);
         vm.stopPrank();
 
         assertEq(mockERC.balanceOf(userDstAddress), dstAmount, "User destination did not receive ERC20 tokens");
@@ -210,140 +161,71 @@ contract PaymentRegistryTest is Test {
     }
 
     function testFulfillmentFailsOnEthSentOnERC20() public {
+        PaymentRegistry.OrderFulfillmentData[] memory orders = _createDefaultErc20OrdersArray(dstAmount);
         // attach eth while doing an erc20 transfer
-        vm.prank(MMAddress);
-        vm.expectRevert("ERC20: msg.value must be 0");
-        paymentRegistry.mostFulfillOrder{value: dstAmount}(
-            orderId,
-            srcEscrow,
-            userSrcAddress,
-            userDstAddress,
-            expirationTimestamp,
-            srcToken,
-            srcAmount,
-            address(mockERC),
-            dstAmount,
-            srcChainId,
-            mmSrcAddress
-        );
+        vm.startPrank(MMAddress);
+        mockERC.approve(address(paymentRegistry), dstAmount);
+        paymentRegistry.mostFulfillOrders{value: dstAmount}(orders);
+        vm.stopPrank();
+
+        assertEq(mockERC.balanceOf(userDstAddress), dstAmount, "User destination did not receive ERC20 tokens");
+        assertEq(mockERC.balanceOf(MMAddress), 10 ether - dstAmount, "MM ERC20 balance did not decrease correctly");
+        assertEq(address(paymentRegistry).balance, dstAmount, "PaymentRegistry should have received the ETH");
     }
 
     function testFulfillmentFailsOnERCAmountZero() public {
+        PaymentRegistry.OrderFulfillmentData[] memory orders = _createDefaultErc20OrdersArray(0);
         // the dst amount is zero
         vm.prank(MMAddress);
         vm.expectRevert("ERC20: Amount must be > 0");
-        paymentRegistry.mostFulfillOrder{value: 0}(
-            orderId,
-            srcEscrow,
-            userSrcAddress,
-            userDstAddress,
-            expirationTimestamp,
-            srcToken,
-            srcAmount,
-            address(mockERC),
-            0,
-            srcChainId,
-            mmSrcAddress
-        );
+        paymentRegistry.mostFulfillOrders{value: 0}(orders);
     }
 
     function testSendingMoreERCThanInBalance() public {
+        PaymentRegistry.OrderFulfillmentData[] memory orders = _createDefaultErc20OrdersArray(100 ether);
         vm.startPrank(MMAddress);
         mockERC.approve(address(paymentRegistry), 100 ether);
         vm.expectRevert();
-        paymentRegistry.mostFulfillOrder{value: 0}(
-            orderId,
-            srcEscrow,
-            userSrcAddress,
-            userDstAddress,
-            expirationTimestamp,
-            srcToken,
-            srcAmount,
-            address(mockERC),
-            100 ether,
-            srcChainId,
-            mmSrcAddress
-        );
+        paymentRegistry.mostFulfillOrders{value: 0}(orders);
         vm.stopPrank();
     }
 
     function testRevertTransferToNonPayableContract() public {
         // Deploy a contract that cannot receive ETH
         NonPayableReceiver nonPayable = new NonPayableReceiver();
+        PaymentRegistry.OrderFulfillmentData[] memory orders =
+            _createOrdersArray(dstTokenETH, dstAmount, address(nonPayable), expirationTimestamp);
         vm.prank(MMAddress);
         vm.expectRevert("Native ETH: Transfer failed");
-        paymentRegistry.mostFulfillOrder{value: dstAmount}(
-            orderId,
-            srcEscrow,
-            userSrcAddress,
-            address(nonPayable),
-            expirationTimestamp,
-            srcToken,
-            srcAmount,
-            dstTokenETH,
-            dstAmount,
-            srcChainId,
-            mmSrcAddress
-        );
+        paymentRegistry.mostFulfillOrders{value: dstAmount}(orders);
     }
 
     function testERC20FailsWithoutApproval() public {
+        PaymentRegistry.OrderFulfillmentData[] memory orders = _createDefaultErc20OrdersArray(dstAmount);
         vm.prank(MMAddress);
         vm.expectRevert();
-        paymentRegistry.mostFulfillOrder{value: 0}(
-            orderId,
-            srcEscrow,
-            userSrcAddress,
-            userDstAddress,
-            expirationTimestamp,
-            srcToken,
-            srcAmount,
-            address(mockERC),
-            dstAmount,
-            srcChainId,
-            mmSrcAddress
-        );
+        paymentRegistry.mostFulfillOrders{value: 0}(orders);
     }
 
     function testRevertTransferWithBrokenERC20() public {
         BrokenERC20 broken = new BrokenERC20("FailToken", "FAIL");
         broken.mint(MMAddress, 10 ether);
 
+        PaymentRegistry.OrderFulfillmentData[] memory orders =
+            _createOrdersArray(address(broken), dstAmount, userDstAddress, expirationTimestamp);
         vm.startPrank(MMAddress);
         broken.approve(address(paymentRegistry), dstAmount);
         vm.expectRevert();
-        paymentRegistry.mostFulfillOrder{value: 0}(
-            orderId,
-            srcEscrow,
-            userSrcAddress,
-            userDstAddress,
-            expirationTimestamp,
-            srcToken,
-            srcAmount,
-            address(broken),
-            dstAmount,
-            srcChainId,
-            mmSrcAddress
-        );
+        paymentRegistry.mostFulfillOrders{value: 0}(orders);
         vm.stopPrank();
     }
 
     function testFulfillmentFailsOnEthAmountZero() public {
+        PaymentRegistry.OrderFulfillmentData[] memory orders =
+            _createOrdersArray(dstTokenETH, 0, userDstAddress, expirationTimestamp);
         vm.prank(MMAddress);
         vm.expectRevert("Native ETH: Amount must be > 0");
-        paymentRegistry.mostFulfillOrder{value: 0}(
-            orderId,
-            srcEscrow,
-            userSrcAddress,
-            userDstAddress,
-            expirationTimestamp,
-            srcToken,
-            srcAmount,
-            dstTokenETH,
-            0, // dstAmount = 0
-            srcChainId,
-            mmSrcAddress
-        );
+        paymentRegistry.mostFulfillOrders{value: 0}(orders);
     }
 }
 
