c7ff3171247799c9c7a47c244abd1d4f65cedeed
jackchinski <marcin.jacz@gmail.com>
2025-08-25 11:47:01 -0400

    chore: shift to satellite

--- src/contracts/Escrow.sol
+++ src/contracts/Escrow.sol
@@ -1,8 +1,8 @@
 // SPDX-License-Identifier: GPL-3.0
 pragma solidity ^0.8.26;
 
-import {ModuleTask, ModuleCodecs} from "lib/herodotus-evm-v2/src/libraries/internal/data-processor/ModuleCodecs.sol";
-import {IDataProcessorModule} from "lib/herodotus-evm-v2/src/interfaces/modules/IDataProcessorModule.sol";
+import {ModuleTask, ModuleCodecs} from "satellite/solidity/src/libraries/internal/data-processor/ModuleCodecs.sol";
+import {IDataProcessorModule} from "satellite/solidity/src/interfaces/modules/IDataProcessorModule.sol";
 import {ReentrancyGuard} from "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
 import {SafeERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
